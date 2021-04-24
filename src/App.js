2fa242bd7d11e3b99a7373213e04d2ac496cd007
Sairoden <odingandarosa@gmail.com>
2021-04-24 21:01:14 +0800

    Added particles background and fixed some design

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
 import Rank from "./Components/Rank/Rank";
 import "./App.css";
+import Particles from "react-particles-js";
 
 class App extends Component {
+  particleOptions = {
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+    },
+  };
+
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={this.particleOptions} />
         <Navigation></Navigation>
         <Logo></Logo>
         <Rank></Rank>
